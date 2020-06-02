34ce3612d4fa05cd99348c55ef2bc7641174ac3f
cc13446 <1344613920@qq.com>
2020-06-02 21:53:33 +0800

    20200602

--- src/Dao/LogDao.java
+++ src/Dao/LogDao.java
@@ -1,5 +1,43 @@
 package Dao;
 
+import java.util.Date;
+
+enum ScheduleType {
+    NEW_REQUEST, CHANGE_TEMP, CHANGE_FAN_SPEED, CLOSE
+}
+enum Mode {
+    HOT, COLD, FAN
+}
 public class LogDao {
+    public boolean storeLog(int roomId, ScheduleType scheduleType, Mode mode, double fanSpeed, double current,
+                     double targetTemp, double fee, double feeRate){
+        return true;
+    }
+
+    public int QueryTotalFee(int roomId, Date dateIn, Date dateOut){
+        return 1;
+    }
+
+    public int QueryTurnTimes(int roomId, Date date){
+        return 1;
+    }
+    public Date QueryUseTime(int roonmId, Date date){
+        return new Date();
+    }
+    public int QuerySchedulerTimes(int roomId, Date date){
+        return 1;
+    }
+    public int QueryDetailNumbers(int roomId, Date date){
+        return 1;
+    }
+    public int QueryChangeTempTimes(int roomId, Date date){
+        return 1;
+    }
+    public int QueryChangeFanTimes(int roomId, Date date){
+        return 1;
+    }
+    public int QueryLog(int roomId, Date dateIn, Date dateOut){
+        return 1;
+    }
 
 }
--- src/app/Servant.java
+++ src/app/Servant.java
@@ -1,4 +1,83 @@
 package app;
-
+enum Mode {
+    HOT, COLD, FAN
+}
+enum FanSpeed {
+    LOW, MEDIUM, HIGH
+}
 public class Servant {
+    private int roomId;
+    private Mode mode;
+    private FanSpeed fanSpeed;
+    private double currentTemp;
+    private double targetTemp;
+    private double fee;
+    private double feeRate;
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public Mode getMode() {
+        return mode;
+    }
+
+    public void setMode(Mode mode) {
+        this.mode = mode;
+    }
+
+    public FanSpeed getFanSpeed() {
+        return fanSpeed;
+    }
+
+    public void setFanSpeed(FanSpeed fanSpeed) {
+        this.fanSpeed = fanSpeed;
+    }
+
+    public double getCurrentTemp() {
+        return currentTemp;
+    }
+
+    public void setCurrentTemp(double currentTemp) {
+        this.currentTemp = currentTemp;
+    }
+
+    public double getTargetTemp() {
+        return targetTemp;
+    }
+
+    public void setTargetTemp(double targetTemp) {
+        this.targetTemp = targetTemp;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    public double getFeeRate() {
+        return feeRate;
+    }
+
+    public void setFeeRate(double feeRate) {
+        this.feeRate = feeRate;
+    }
+    public boolean changeRoomTemp(){
+        return true;
+    }
+
+    public boolean storeLog(){
+        return true;
+    }
+
+    public boolean changeFee(){
+        return true;
+    }
 }
