e2b76d8530ca963e4a0b2531db5737a1534df181
StupidRabbit29 <2922282053@qq.com>
2020-06-02 16:50:28 +0800

    202006021650

--- src/Controller/StartUpController.java
+++ src/Controller/StartUpController.java
@@ -1,5 +1,5 @@
 package Controller;
 
-public class StartUpController{
+public class StartUpController {
 
 }
--- src/Controller/UseController.java
+++ src/Controller/UseController.java
@@ -1,5 +1,5 @@
 package Controller;
 
-public class UseController{
+public class UseController {
 
 }
