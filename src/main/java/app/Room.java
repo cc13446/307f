83dcccb705ef4b7ab63cf6fd85578ebaac8af73a
StupidRabbit29 <2922282053@qq.com>
2020-06-04 01:07:05 +0800

    fb 2020/6/4 1:06

--- src/main/java/app/Room.java
+++ src/main/java/app/Room.java
@@ -1,5 +1,43 @@
 package app;
 
+import Enum.State;
+
 public class Room {
+    private State state;
+    private int roomID;
+    private double CurrentTemp;
+    private double Fee;
+
+    private State getState() {
+        return state;
+    }
+
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public double getFee() {
+        return Fee;
+    }
+
+    public double getCurrentTemp() {
+        return CurrentTemp;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void setRoomID(int roomID) {
+        this.roomID = roomID;
+    }
+
+    public void setFee(double fee) {
+        Fee = fee;
+    }
+
+    public void setCurrentTemp(double currentTemp) {
+        CurrentTemp = currentTemp;
+    }
 
 }
