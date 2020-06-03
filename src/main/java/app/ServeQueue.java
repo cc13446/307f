37e29d857331f3d2ccd7aeab93d5c89c8bc69eb0
cc13446 <1344613920@qq.com>
2020-06-03 20:30:41 +0800

    cc 2020/6/3 20:30

---
+++ src/main/java/app/ServeQueue.java
@@ -0,0 +1,79 @@
+package app;
+
+import java.util.LinkedList;
+import java.util.List;
+import Enum.FanSpeed;
+
+public class ServeQueue {
+
+    public List<Request> serveRequestList;
+
+    public ServeQueue(){
+        serveRequestList=new LinkedList<Request>();
+    }
+
+    public int size(){
+        return serveRequestList.size();
+    }
+
+    public void addRequest(Request request){
+        serveRequestList.add(request);
+    }
+
+    public boolean removeRequest(int roomId){
+        for (Request req:serveRequestList){
+            if(roomId==req.getRoomId()){
+                serveRequestList.remove(req);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Request getSlowestFanSpeedRequest(){
+        if (0==serveRequestList.size())
+            return null;
+        Request worst=serveRequestList.get(0);
+        for (Request req:serveRequestList){
+            if(req.getFanSpeed().compareTo(worst.getFanSpeed())<0){
+                worst=req;
+            }
+        }
+        return worst;
+    }
+
+    public void replaceRequest(Request request){
+        int outReqId=getSlowestFanSpeedRequest().getRoomId();
+        removeRequest(outReqId);
+        addRequest(request);
+    }
+
+    public Request findRequest(int roomId){
+        for (Request req:serveRequestList){
+            if(roomId==req.getRoomId()){
+                return req;
+            }
+        }
+        return null;
+    }
+
+    public boolean changeRequestTemp(int roomId, double temp){
+        for (Request req:serveRequestList){
+            if(roomId==req.getRoomId()){
+                req.setTargetTemp(temp);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean changeRequestFanSpeed(int roomId, FanSpeed fanSpeed){
+        for (Request req:serveRequestList){
+            if(roomId==req.getRoomId()){
+                req.setFanSpeed(fanSpeed);
+                return true;
+            }
+        }
+        return false;
+    }
+}
