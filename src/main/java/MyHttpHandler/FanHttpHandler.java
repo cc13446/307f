bc35dd1a9079e11b3c70f11692cac8b14d0e7c6b
cc13446 <1344613920@qq.com>
2020-06-04 19:51:55 +0800

    cc 2020/6/4 19:51

--- src/main/java/MyHttpHandler/FanHttpHandler.java
+++ src/main/java/MyHttpHandler/FanHttpHandler.java
@@ -1,32 +1,16 @@
-package myHttp;
-
-import java.io.*;
-import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
+package MyHttpHandler;
 
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
 import net.sf.json.JSONObject;
 
+import java.io.*;
 
-public class HttpTest {
-    public static void main(String[] args) throws IOException {
-        InetSocketAddress addr = new InetSocketAddress(8080);
-        HttpServer server = HttpServer.create(addr, 0);
-
-        server.createContext("/", new MyHandler());
-        server.setExecutor(Executors.newCachedThreadPool());
-        server.start();
-        System.out.println("Server is listening on port 8080" );
-    }
-}
-
-class MyHandler implements HttpHandler {
+public class FanHttpHandler implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         String requestMethod = exchange.getRequestMethod();
-        if (requestMethod.equalsIgnoreCase("GET")) {
+        if (requestMethod.equalsIgnoreCase("POST")) {
             InputStream in = exchange.getRequestBody();
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
             String str = reader.readLine();
