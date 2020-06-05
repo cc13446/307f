b920b73f1781a70b13578ba449d5d7dba2a58ebe
lxx <lxx13273780760@outlook.com>
2020-06-05 20:16:01 +0800

    lxx 2020/6/5 20:07

--- src/main/java/MyHttpHandler/FeeHttpHandler.java
+++ src/main/java/MyHttpHandler/FeeHttpHandler.java
@@ -1,10 +1,13 @@
 package MyHttpHandler;
 
 import Controller.UseController;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import net.sf.json.JSONObject;
+import Enum.*;
 
-import java.io.IOException;
+import java.io.*;
 
 public class FeeHttpHandler implements HttpHandler {
     private UseController useController;
@@ -16,6 +19,29 @@ public class FeeHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        String requestMethod = exchange.getRequestMethod();
+        if (requestMethod.equalsIgnoreCase("POST")) {
+            System.out.println(exchange.getRequestURI());
+            InputStream in = exchange.getRequestBody();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String str = reader.readLine();
+            JSONObject temp = JSONObject.fromObject(str);
+            System.out.println("收到：" + temp);
+            int id=temp.getInt("id");
+            int fanSpeed=temp.getInt("fanSpeed");
 
+            useController.changeFanSpeed(id, FanSpeed.values()[fanSpeed]);
+
+            Headers responseHeaders = exchange.getResponseHeaders();
+            responseHeaders.set("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, 0);
+
+            OutputStream responseBody = exchange.getResponseBody();
+            JSONObject json=new JSONObject();
+            json.put("status",0);
+
+            responseBody.write(json.toString().getBytes());
+            responseBody.close();
+        }
     }
 }
